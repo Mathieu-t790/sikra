972cc44198abda23e86d3ad7e3ff9b128f1f877d
Tafita Mathieu <199898191+Mathieu-bot@users.noreply.github.com>
2026-07-12 23:50:34 +0300

    Registration (#4)
    * feat: add business tables for course subscription system
    
    * feat: add domain models for course subscription system
    
    * feat: add JPA entities for business tables
    
    * feat: add JPA repositories
    
    * feat: add mappers between domain models and JPA entities
    
    * feat: add business services for course subscription
    
    * feat: add async email confirmation event and worker
    
    * feat: add DTOs for subscription endpoints
    
    * feat: add subscription endpoint and global error handler
    
    * chore: remove template files
    
    * chore: make gradlew executable
    
    * feat: add JPA validation annotations on entity fields
    
    * chore: format
    
    * chore: remove useless blank
    
    * feat: add unit and integration tests for subscription flow
    
    * chore: format code
    
    * fix: restore missing SikraApplication import in handlers
    
    * feat: add seed data and worker unit test
    
    * chore: align code
    
    ---------
    
    Co-authored-by: Mathieu-bot <mathieu-bot@users.noreply.github.com>

---
+++ src/main/resources/db/migration/V42_4__Seed_test_data.sql
@@ -0,0 +1,9 @@
+insert into "user" (id, first_name, last_name, user_name, email)
+values ('a0000000-0000-0000-0000-000000000001', 'Tafita Mathieu', 'RAZAFIMANDIMBY', 'mata', 'hei.tafita.2@gmail.com');
+
+insert into course (id, title)
+values ('b0000000-0000-0000-0000-000000000001', 'Prog4');
+insert into course (id, title)
+values ('b0000000-0000-0000-0000-000000000002', 'Projet1');
+insert into course (id, title)
+values ('b0000000-0000-0000-0000-000000000003', 'Web3');
